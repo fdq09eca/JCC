b69e6912edae77c492e8e50c5a9f72b2b1b724ac
Chirs Lam <fdq09eca@gmail.com>
2021-01-20 01:23:24 +0000

    init

--- ex/my_strncpy.c
+++ ex/my_strncpy.c
@@ -1,19 +1,37 @@
 #include <stdio.h>
 
-int main()
+char *my_strncpy(char *dest, char *src, int n)
 {
-    char dest[100];
-    printf("%s\n", my_strncpy(dest, "123456789", 5));
-}
 
-char* my_strnspy(char* dest, char* src, int n) {
-    
-    char* rtn = dest;
-    int c = 0;
-    while (*src) {
-        *dest = *src;
+    char *rtn = dest;
+    while (n--)
+    {
+        *dest = *src ? *src : 0;
         dest++;
         src++;
     }
+
     return rtn;
+}
+
+int main()
+{
+    char src[] = "hi";
+    char dest[6] = "abcdef";  // no null terminator
+    my_strncpy(dest, src, 5); // writes five characters 'h', 'i', '\0', '\0', '\0' to dest
+    printf("strncpy(dest, src, 5) to a 6-byte dest gives : ");
+    for (size_t n = 0; n < sizeof dest; ++n)
+    {
+        char c = dest[n];
+        c ? printf("'%c' ", c) : printf("'\\0' ");
+    }
+    printf("\nstrncpy(dest2, src, 2) to a 2-byte dst gives : ");
+    char dest2[2];
+    my_strncpy(dest2, src, 2); // truncation: writes two characters 'h', 'i', to dest2
+    for (size_t n = 0; n < sizeof dest2; ++n)
+    {
+        char c = dest2[n];
+        c ? printf("'%c' ", c) : printf("'\\0' ");
+    }
+    printf("\n");
 }
