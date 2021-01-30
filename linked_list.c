d8f480aae24fa7e4f050d4e3d05b7e1ba9d4e318
Chirs Lam <fdq09eca@gmail.com>
2021-01-30 03:26:00 +0000

    init

---
+++ linked_list.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+//== Node ==
+typedef struct
+{
+    int v;
+    Node *next;
+} Node;
+
+//== LinkedList ==
+
+typedef struct
+{
+    Node * head;
+    count 
+} LinkedList;
+
+// ===============
+int main()
+{
+    return 0;
+}
