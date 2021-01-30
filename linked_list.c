dfc321a7cf04d20ed79bd24608b0ac32fe1b25e9
Chirs Lam <fdq09eca@gmail.com>
2021-01-30 04:51:57 +0000

    init & release

--- linked_list.c
+++ linked_list.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 //== Node ==
 typedef struct
 {
@@ -6,14 +7,59 @@ typedef struct
     Node *next;
 } Node;
 
+void node_init(Node *node)
+{
+    node->v = 0;
+    node->next = NULL;
+}
+
+void node_release(Node *node)
+{
+    node->v = 0;
+    node->next = NULL;
+    free(node);
+}
+
 //== LinkedList ==
 
 typedef struct
 {
-    Node * head;
-    count 
+    int count;
+    Node *head;
 } LinkedList;
 
+void lls_init(LinkedList *lli, Node *head)
+{
+    lli->count = 0;
+    lli->head = head;
+}
+
+void lls_release(LinkedList *lli)
+{
+    lli->count = 0;
+
+    // Node **n = &lli->head;
+    // while (*n) {
+    //     n = (*n)->next;
+    //     node_release(*n);
+    // }
+
+    Node *h = lli->head;
+    Node *t = NULL;
+    while (h)
+    {
+        t = h->next;
+        node_release(h);
+        h = t;
+        t = NULL;
+    }
+
+    // for (Node *h = lli->head, *t; h != NULL; h = t)
+    // {
+    //     t = h->next;
+    //     node_release(h);
+    // }
+}
 // ===============
 int main()
 {
