9cea49c1db9a01b23a7ad0636e6088e527bda25e
Chirs Lam <fdq09eca@gmail.com>
2021-02-08 01:04:10 +0000

    dll redo

--- doubly_linked_list.cpp
+++ doubly_linked_list.cpp
@@ -1,46 +1,264 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#define TEST(EXPR) \
-    do { \
+#include <iostream>
+
+#define TEST(EXPR)                                                                  \
+    do                                                                              \
+    {                                                                               \
         printf("[%s] Line %3d: %s\n", ((EXPR) ? " OK " : "FAIL"), __LINE__, #EXPR); \
-    } while(0) \
-//-------------------
-size_t n_malloc = 0;
+    } while (0) //-------------------
 
-void* my_malloc(size_t n){
-    n_malloc ++;
-    return malloc(n);
-}
-
-void my_free(void* p) {
-    n_malloc--;
-    free(p);
-}
-
-//== Node ==
-struct _Node;
-typedef struct _Node Node;
-
-struct _Node
+class Node
 {
+private:
+    Node *prev;
+    Node *next;
+    friend class DoublyLinkedList;
+
+public:
     int v;
-    Node* prev;
-    Node* next;
+
+    Node() : prev(nullptr), next(nullptr){};
+
+    Node(int v_) : prev(nullptr), next(nullptr), v(v_){};
+
+    ~Node()
+    {
+        std::cout << "Node->v: " << v << " destructed\n";
+    }
 };
 
-typedef struct
+class DoublyLinkedList
 {
-    size_t len;
+public:
     Node *head;
     Node *tail;
-} LinkedList;
+    size_t len;
 
-// object ownership!!
-// aim: 90%
-// C++
+    DoublyLinkedList() : head(nullptr), tail(nullptr), len(0){};
 
-int main() {
-    TEST(my_malloc == 0);
+    ~DoublyLinkedList(){
+        clear();
+        std::cout << "LinkedList destructed.\n";
+    }
+
+    void clear(){
+        Node* p;
+        while ((p = pop_head())) {
+            delete p;
+        }
+        std::cout << "LinkedList cleared.\n";
+    }
+
+    Node* insert(int v_) {
+        return insert_after(nullptr, v_);
+    }
+
+    Node* append(int v_) {
+        return insert_after(tail, v_);
+    }
+
+    Node* find(int v_) {
+        for (Node* p = head; p ; p=p->next) {
+            if (p->v == v_) return p;
+        }
+        return nullptr;
+    }
+
+    Node* pop_head() {
+        return remove(head);
+    }
+    
+    Node* pop_tail() {
+        return remove(tail);
+    }
+
+    Node* remove(Node* target_node){
+        if (!target_node) {
+            return nullptr;
+        }
+        
+        if (target_node->prev) {
+            target_node->prev->next = target_node->next;    
+        } else {
+            head = target_node->next;
+        }
+
+        if (target_node->next) {
+            target_node->next->prev = target_node->prev;
+        } else {
+            tail = target_node->prev;
+        }
+        
+        target_node->prev = nullptr;
+        target_node->next = nullptr;
+        len --;
+        
+        return target_node;
+    }
+    
+
+    Node* insert_after(Node *target_node, Node* new_node)
+    {
+        if (!target_node)
+        {
+            if (head) {
+                new_node->next = head;
+                head->prev = new_node;
+            } else {
+                tail = new_node;
+            }
+            head = new_node;
+        } else {
+            if (target_node->next) {
+                // T <- N <- Tn: 102, 104
+                // T -> N -> Tn: 103, 105
+                new_node->prev = target_node;
+                new_node->next = target_node->next;
+                target_node->next->prev = new_node;
+                target_node->next = new_node;
+            } else {
+                // tail
+                new_node->prev = target_node;
+                target_node->next = new_node;
+                tail = new_node;
+
+            }
+        }
+        len++;
+        return new_node;
+        
+    }
+
+    Node* insert_after(Node *target_node, int v_)
+    {
+        Node *new_node = new Node(v_);
+        return insert_after(target_node, new_node);
+    }
+    
+    Node* insert_after(int t_, int v_)
+    {
+        Node *new_node = new Node(v_);
+        Node* target_node = find(t_);
+        return insert_after(target_node, new_node);
+    }
+    
+    void print()
+    {
+        std::cout <<"next: ";
+        Node *p = head;
+        while (p)
+        {
+            std::cout << p->v << "->";
+            p = p->next;
+        }
+        std::cout << "|<" << len << ">\n";
+    }
+
+    void print_r()
+    {
+        std::cout <<"prev: ";
+        Node *p = tail;
+        while (p)
+        {
+            std::cout << p->v << "->";
+            p = p->prev;
+        }
+        std::cout << "|<" << len << ">\n";
+    }
+};
+
+int main()
+{
+    DoublyLinkedList dll;
+    
+    std::cout<<"==TEST PUSH HEAD ==\n";
+    for (int i = 1; i <= 10; i++)
+    {
+        dll.insert_after(nullptr, i);
+    }
+    dll.print();
+    dll.print_r();
+    
+    dll.clear();
+    
+    std::cout<<"== TEST PUSH TAIL ==\n";
+    for (int i = 1; i <= 10; i++)
+    {
+        dll.insert_after(dll.tail, i);
+    }
+    dll.print();
+    dll.print_r();
+    dll.clear();
+    
+    std::cout<<"== TEST INSERT AFTER ==\n";
+    Node* p = nullptr;
+    
+    for (int i = 1; i <= 10; i++)
+    {
+
+        p = dll.insert_after(p, i);
+    }
+
+    dll.print();
+    dll.print_r();
+    dll.clear();
+
+    std::cout<<"== TEST INSERT MIDDLE ==\n";
+    p = dll.insert(0);
+    dll.append(2);
+    dll.insert_after(p, 1);
+    dll.print();
+    dll.print_r();
+    dll.clear();
+
+    std::cout<<"== TEST POP HEAD/TAIL ==\n";
+    p = dll.insert(0);
+    dll.pop_head();
+    dll.print();
+    dll.pop_head();
+    dll.print();
+    dll.pop_tail();
+    dll.print();
+    
+    p = dll.append(0);
+    dll.pop_tail();
+    dll.print();
+    dll.pop_head();
+    dll.print();
+    dll.pop_head();
+    dll.print();
+    dll.print();
+
+    // dll.append(2);
+    // dll.insert_after(p, 1);
+    // dll.print_r();
+    
+
+    // std::cout<<"== TEST FIND ==\n";
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     TEST(dll.find(i) != nullptr);
+    // }
+    
+    // for (int i = 0; i < 11; i++) {
+    //     dll.insert_after(dll.tail, i);
+    // }
+    // dll.print();
+    // dll.print_r();
+    
+    // std::cout<<"== TEST POP HEAD ==\n";
+    // dll.remove(dll.head);
+    // dll.print();
+    // dll.print_r();
+    // std::cout<<"== TEST POP TAIL ==\n";
+    // dll.remove(dll.tail);
+    // dll.print();
+    // dll.print_r();
+
+    // std::cout<<"== TEST clear ==\n";
+    // dll.clear();
+    // dll.print();
+    // dll.print_r();
+
+
     return 0;
 }
